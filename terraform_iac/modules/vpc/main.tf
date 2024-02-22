67d8ccb89e86b8f087f1eb54a188e2f45e498c7c
anoopkrrish07 <anoopkrrish07@gmail.com>
2024-02-23 00:23:11 +0100

    feat(workflow): Updating github workflow and checov scans

--- terraform_iac/modules/vpc/main.tf
+++ terraform_iac/modules/vpc/main.tf
@@ -3,8 +3,9 @@ resource "random_id" "id" {
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.5.0"
+  # source  = "terraform-aws-modules/vpc/aws"
+  # version = "~> 5.5.0"
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=f7f84f70ad6db36e4eae0273bf08ed2ffd0a69c4"
 
   name = "${var.env}-${var.group}-${var.app}-${random_id.id.hex}"
   cidr = var.vpc_cidr
